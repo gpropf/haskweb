19718df1549dc17b767dac70d175d88ccd3551c2
Greg Propf <gpropf@gmail.com>
2018-04-23 20:28:21 -0700

    Added cabal.project files for GHC and GHCJS

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -1,4 +1,20 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+
+
 module Main where
 
+import Reflex.Dom
+
+
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = mainWidget $ el "div" $ do
+  el "p" $ text "Haskweb Frontend, type something in the textbox..."
+  ga <- graphicArea $ return ()
+  t <- textInput def
+  el "div" $ dynText $ _textInput_value t
+  return ()
+
+graphicArea :: MonadWidget t m => m a -> m a
+graphicArea = el "svg" 
