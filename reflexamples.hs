d524eb556d0d78e361c00c76aa47a431d5ce136e
Greg Propf <gpropf@gmail.com>
2018-04-23 17:30:51 -0700

    Made a new div for the dynText

--- reflexamples.hs
+++ reflexamples.hs
@@ -7,7 +7,7 @@ main = mainWidget $ el "div" $ do
   el "p" $ text "Welcome to Haskweb, my collection of Haskell demos using GHCJS and the Reflex framework."
   ga <- graphicArea $ return ()
   t <- textInput def
-  dynText $ _textInput_value t
+  el "div" $ dynText $ _textInput_value t
   return ()
 
 graphicArea :: MonadWidget t m => m a -> m a
