68a140a61630f0b9254945a9d3fe6b0e36223baa
Greg Propf <gpropf@gmail.com>
2018-04-26 15:01:42 -0700

    Added a button and some more dynamic test content

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -5,8 +5,9 @@ module Main where
 
 --import Control.Monad.State
 import Data.Monoid
+import Data.Typeable
 --import Data.IntMap (IntMap, assocs, elems, empty, fromList, size, singleton)
-import Data.Text (Text)
+import Data.Text (Text, pack)
 --import qualified Data.Text as T
 --import Data.Vector (Vector)
 --import qualified Data.Vector as Vector
@@ -19,14 +20,22 @@ import Reflex.Dom
 
 
 main :: IO ()
-main = mainWidget $ el "span" $ do 
+main = mainWidget $ el "div" $ do
   el "p" $ text "Haskweb Frontend (V2), type something in the textbox..."
-  ga <- graphicArea $ return ()
+  --ga <- graphicArea $ return ()
   t <- textInput def
-  el "div" $ dynText $ _textInput_value t
+  b1 <- button "Push Me!"
+  el "div" $ dynText $ _textInput_value t;
+  -- let pe = performEvent (putStrLn "I'm happening!!")
   (cnvs, _) <- elAttr' "canvas" ("width" =: "600" <> "height" =: "400") blank
-  return ()
+  --return ();
+  el "div" $ dynText $ fmap (pack . show) $ _textInput_hasFocus t
+  let kpe = fmap (pack . show) $ _textInput_keypress t
+  kpd <- holdDyn "None" kpe 
+  dynText kpd
   
+  
+
 
 graphicArea :: (MonadWidget t m) => m a -> m a
 graphicArea = el "svg" 
