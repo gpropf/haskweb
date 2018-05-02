5e0774954cb8af1cdab7392221b8924dae68ae9e
Greg Propf <gpropf@gmail.com>
2018-05-02 15:14:48 -0700

    Trying to adapt the circle demo code - doesn't do much yet

--- frontend/src/Main.hs
+++ frontend/src/Main.hs
@@ -3,39 +3,70 @@
 
 module Main where
 
---import Control.Monad.State
 import Data.Monoid
 import Data.Typeable
---import Data.IntMap (IntMap, assocs, elems, empty, fromList, size, singleton)
-import Data.Text (Text, pack)
---import qualified Data.Text as T
---import Data.Vector (Vector)
---import qualified Data.Vector as Vector
+import Data.Text (Text, pack, unpack)
 import Reflex.Dom
---import Data.FastMutableIntMap
---import System.Random
+import System.Random
+import Data.Map (Map, fromList)
 
--- import Data.Map
 
+width  = 400
+height = 300
+
+svgNamespace = Just "http://www.w3.org/2000/svg"
+
+svgAttrs = fromList [ ( "viewBox" , pack (
+                                        show (-width / 2) 
+                                     ++ " " ++ show (-height / 2) 
+                                     ++ " " ++ show width 
+                                     ++ " " ++ show height) )
+                    , ( "width" ,    (pack . show) width)
+                    , ( "height" ,   (pack . show) height)
+                    , ( "style", "border: 1px solid black")
+                    ] 
 
 
 main :: IO ()
 main = mainWidget $ el "div" $ do
   el "p" $ text "Haskweb Frontend (V2), type something in the textbox..."
-  --ga <- graphicArea $ return ()
-  t <- textInput def
+  tix <- textInput $ def { _textInputConfig_initialValue = "50" }
+  tiy <- textInput $ def { _textInputConfig_initialValue = "40" }
+  dtx <- dynText $ _textInput_value tix
+
+--  dty <-
+  el "div" $ dynText $ _textInput_value tix
+  el "div" $ dynText $ _textInput_value tiy
+--  (cnvs, _) <- elAttr' "canvas" ("width" =: "600" <> "height" =: "400") blank
+  el "div" $ dynText $ fmap (pack . show) $ _textInput_hasFocus tix
   b1 <- button "Push Me!"
-  el "div" $ dynText $ _textInput_value t;
-  -- let pe = performEvent (putStrLn "I'm happening!!")
-  (cnvs, _) <- elAttr' "canvas" ("width" =: "600" <> "height" =: "400") blank
-  --return ();
-  el "div" $ dynText $ fmap (pack . show) $ _textInput_hasFocus t
-  let kpe = fmap (pack . show) $ _textInput_keypress t
+
+  let kpe = fmap (pack . show) $ _textInput_keypress tix
   kpd <- holdDyn "None" kpe 
   dynText kpd
-  
-  
+  let xStr = value tix
+      yStr = value tiy
 
-
+--  x <- mapM (read . unpack) xStr
+  --y <- mapM (read . unpack) yStr
+  --ourCircle <- fmap $ (mapM showCircle) [(("Green", 45), (x,y))]
+  elDynAttrNS' svgNamespace "svg" (constDyn svgAttrs) $ blank
+  return ()
+      
+  
+--stringToCircle = mapM showCircle
+                 
 graphicArea :: (MonadWidget t m) => m a -> m a
 graphicArea = el "svg" 
+
+
+showCircle :: MonadWidget t m => ((String,Double), (Double,Double)) -> m ()
+showCircle ((color, radius), (x, y)) = do
+    let circleAttrs = fromList [ ( "cx", (pack . show) x)
+                               , ( "cy", (pack . show) y)
+                               , ( "r",  (pack . show) radius)
+                               , ( "style",  pack $ "fill:" ++ color) ] 
+
+    elDynAttrNS' svgNamespace "circle" (constDyn circleAttrs) $ return ()
+
+    return ()
